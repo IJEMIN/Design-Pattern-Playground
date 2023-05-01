c26981bdb3bd34611b15baec0c57600a849baa95
i_jemin <jemin.lee@unity3d.com>
2023-05-01 14:16:46 +0900

    Create entrance point

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Program
+{
+    public static void Main()
+    {
+        // select pattern to demonstrate
+        Console.WriteLine("Select pattern to demonstrate:");
+        Console.WriteLine("1. Memento");
+        Console.WriteLine("2. Iterator");
+
+        IDemo demoRunner;
+
+        switch (Console.ReadKey().KeyChar)
+        {
+            case '1':
+                // demoRunner = new MementoPattern.Demo();
+                throw new NotImplementedException();
+                break;
+            case '2':
+                demoRunner = new IteratorPattern.Demo();
+                break;
+            default:
+                Console.WriteLine("Invalid selection");
+                return;
+        }
+
+        demoRunner.Run();
+    }
+}
